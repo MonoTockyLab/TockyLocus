cdd9a2152ed77330154a8b6358bf15b7d5d2f2df
Masahiro Ono (Tocky Lab) <126912013+MonoTockyLab@users.noreply.github.com>
2024-11-17 16:48:33 +0000

    Documentation updated

--- R/internal.R
+++ R/internal.R
@@ -1,3 +1,16 @@
+# Copyright 2024 Masahiro Ono
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #' Create cellcount table for calculating percentage parent
 #' @param x A data frame containing the Timer Angle variable with the column name Angle.
 #' @param percentTimer A logical value for whether Percent Timer data is produced. Default is FALSE and produces Percent Parent data.
